a277a43859fc38cd5910f0f73b9e8a39c8dc6d36
Dean Agan <agandfr@gmail.com>
2020-10-11 15:49:43 +1100

    Simplified solution to valid parentheses

--- lib/valid_parentheses.cpp
+++ lib/valid_parentheses.cpp
@@ -9,26 +9,38 @@ namespace validparentheses
 namespace
 {
     const std::map<char,char> brackets { {'{', '}'}, {'(', ')'}, {'[',']'} };
-
 }
+
+bool ValidParentheses::ContainsValue(char ch) {
+    return std::find_if(begin(brackets), end(brackets), [ch](auto bp) {
+        return bp.second == ch;
+    }) != end(brackets);
+}
+
+bool ValidParentheses::ContainsKey(char ch) {
+    return brackets.find(ch) != end(brackets);
+}
+
+char ValidParentheses::Pop(std::vector<char>& coll) {
+    const auto ch = coll.back();
+    coll.pop_back();
+    return ch;
+}
+
 bool ValidParentheses::IsValid(const std::string& s) {
 
-    if (s.length() % 2U) {
-        return false;
-    }
-
-    std::vector<char> entries;
+    std::vector<char> entries { '\0' };
 
     for(const auto ch : s)
     {
-        if (brackets.find(ch) != end(brackets)) {
-            entries.emplace_back(ch);
-        } else if (!entries.empty() && brackets.at(entries.back()) == ch) {
-            entries.pop_back();
+        if (ContainsKey(ch)) {
+            entries.emplace_back(brackets.at(ch));
+        } else if (ContainsValue(ch) && (ch != Pop(entries))) {
+            return false;
         }
     }
 
-    return entries.empty();
+    return (entries.size() == 1) && (entries.at(0) == '\0');
 }
 
 }  // namespace validparentheses
--- lib/valid_parentheses.h
+++ lib/valid_parentheses.h
@@ -1,11 +1,17 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace validparentheses
 {
     class ValidParentheses {
     public:
         static bool IsValid(const std::string& s);
+
+    private:
+        static bool ContainsValue(char ch);
+        static bool ContainsKey(char ch);
+        static char Pop(std::vector<char>& coll);
     };
 }
